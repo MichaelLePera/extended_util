dd6bdd17dc842aefd7f96a9b9294909508d8eebb
lepmt001 <104551434+Michael-LePera@users.noreply.github.com>
2023-07-09 12:14:27 +0930

    functional

--- src/eutil/search.py
+++ src/eutil/search.py
@@ -3,17 +3,24 @@ from typing import Iterable, Callable, TypeVar, NoReturn
 T = TypeVar('T')
 
 
-def linear(iterable: Iterable[T], predicate: Callable[[T], bool]) -> T:
-    for x in iterable:
-        if predicate(x):
-            return x
-    return None
+class Search:
 
+    @staticmethod
+    def linear(iterable: Iterable[T], predicate: Callable[[T], bool]) -> T:
+        for x in iterable:
+            if predicate(x):
+                return x
+        return None
 
-def binary() -> NoReturn: raise NotImplemented()
+    @staticmethod
+    def binary() -> NoReturn: raise NotImplemented()
 
-def jump() -> NoReturn: raise NotImplemented()
+    @staticmethod
+    def jump() -> NoReturn: raise NotImplemented()
 
-def interpolation() -> NoReturn: raise NotImplemented()
+    @staticmethod
+    def interpolation() -> NoReturn: raise NotImplemented()
 
-def exponential() -> NoReturn: raise NotImplemented()
+    @staticmethod
+    def exponential() -> NoReturn: raise NotImplemented()
+
